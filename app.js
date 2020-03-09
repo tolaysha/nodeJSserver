32b506967b57ba6bda7d4e6a96062f6455d09c5c
Anatoly Kuzmin <tolaykuzay@gmail.com>
2020-03-09 21:46:49 +0300

    example of get

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/about', function (req, res) {
+  res.send('about');
+});
+app.get('/random.text', function (req, res) {
+  res.send('random.text');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
