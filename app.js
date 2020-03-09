80b6ee44939857e24b039e02a11b0b4d8abf9082
Anatoly Kuzmin <tolaykuzay@gmail.com>
2020-03-09 21:47:28 +0300

    multy)

--- app.js
+++ app.js
@@ -10,7 +10,21 @@ app.get('/about', function (req, res) {
 app.get('/random.text', function (req, res) {
   res.send('random.text');
 });
+var cb0 = function (req, res, next) {
+  console.log('CB0');
+  next();
+}
 
+var cb1 = function (req, res, next) {
+  console.log('CB1');
+  next();
+}
+
+var cb2 = function (req, res) {
+  res.send('Hello from C!');
+}
+
+app.get('/example/c', [cb0, cb1, cb2]);
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
